2d8f6d3834dd673eeb10baecb4498eaac6c8dc86
Gaurav Ankit <g.ankit@zohomail.in>
2026-06-26 07:09:27 +0530

    multiple docs,metadata,validation fixed

--- final.py
+++ final.py
@@ -1,7 +1,5 @@
 #better chunking
-
-with open('notes.txt', "r") as file:
-  document = file.read()
+doc_path = [r"notes\os.txt", r"notes\coa.txt", r"notes\rdbms.txt", r"notes\ml.txt"]
 
 #split document - size based
 def chunker(text, size, overlap):
@@ -31,36 +29,58 @@ CHUNKS_PATH = 'chunks.pkl'
 if os.path.exists(INDEX_PATH) and os.path.exists(CHUNKS_PATH):
   index = faiss.read_index(INDEX_PATH)
   with open(CHUNKS_PATH, "rb") as f:
-    chunks = pickle.load(f)
+    data = pickle.load(f)
+    chunks, metadata = data["chunks"], data["metadata"]
 
 else:
-  chunks = chunker(document, 8, 6)
+  chunks = []
+  metadata = []
+
+  for doc in doc_path:
+   with open(doc, "r") as file:
+    document = file.read()
+
+    chunk_doc = chunker(document, 30, 5)
+    for c in chunk_doc:
+      chunks.append(c)
+      metadata.append({
+        "sources" : doc,
+        "chunk" : len(chunks) - 1
+      })
+
   embeddings = []
   for i in chunks:
     embeddings.append(embed.embed_query(i))
 
   vector = np.array(embeddings).astype('float32')
-  index = faiss.IndexFlatL2(len(vector[0]))
+  faiss.normalize_L2(vector)
+  index = faiss.IndexFlatIP(len(vector[0]))
   index.add(vector)
 
   faiss.write_index(index, INDEX_PATH)
   with open(CHUNKS_PATH, "wb") as f:
-    pickle.dump(chunks, f)
+    pickle.dump({"chunks": chunks, "metadata": metadata}, f)
 
-query = input("ask your question :")
+query = input("ask your question: ")
 q_embed = np.array([embed.embed_query(query)]).astype('float32')
+faiss.normalize_L2(q_embed)
 
-k = 2
+k = 5
 distance, indices = index.search(q_embed, k)
 
 top_doc = []
 sources = []
-for j in indices[0]:
-  top_doc.append((chunks[j]))
-  sources.append(f"Chunk [{j}] : {chunks[j]}")
+THRESHOLD = 0.65
+for j,score in zip(indices[0], distance[0]):
+  if score >= THRESHOLD:
+    top_doc.append((chunks[j]))
+    sources.append(metadata[j])
 
-
-context_str = "\n".join(top_doc)
+if not top_doc:
+    print("I don't know")
+    exit()
+else:
+  context_str = "\n".join(top_doc)
 
 
 # combine retrieval + LLM
@@ -89,24 +109,15 @@ Answer:
 
 
 #validation-step to minimize hallucination
-response = chat_model.invoke(prompt)
+response = chat_model.invoke(prompt).content
 
-query_embed = np.array(embed.embed_query(query)).astype('float32')
+response_embed = np.array([embed.embed_query(response)]).astype('float32')
+faiss.normalize_L2(response_embed)
+k = 1
+res_dist, _ = index.search(response_embed, k)
 
-def cosine_similarity(a, b):
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-
-similarities = []
-for c in top_doc:
-   chunk_embed = np.array(embed.embed_query(c)).astype("float32")
-   sim = cosine_similarity(chunk_embed, query_embed)
-   similarities.append(sim)
-
-max_sim = max(similarities)
-THRESHOLD = 0.7 
-
-if max_sim > THRESHOLD:
-  print(response.content)
+if res_dist[0][0] > THRESHOLD:
+  print(response)
   print("\n--- Sources Used ---")
   for s in sources:
       print(s)
